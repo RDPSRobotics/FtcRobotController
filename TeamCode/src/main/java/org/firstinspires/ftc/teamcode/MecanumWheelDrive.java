c7c862dbd646aeef8aba1c9ea24fffd15ecb373f
Shinto <84257893+icytheprogram@users.noreply.github.com>
2026-02-07 20:04:36 -0800

    Changes to the driving code
    - Variables changes
    - Direction of motor
    - Telemetry changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumWheelDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumWheelDrive.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class MecanumWheelDrive extends OpMode {
@@ -16,7 +17,7 @@ public class MecanumWheelDrive extends OpMode {
     double turn;
     double strafe;
 
-    int speed;
+    int speed = 1000;
 
     @Override
     public void init() {
@@ -24,20 +25,28 @@ public class MecanumWheelDrive extends OpMode {
         rBack = hardwareMap.get(DcMotorEx.class, "rBack");
         lFront = hardwareMap.get(DcMotorEx.class, "lFront");
         lBack = hardwareMap.get(DcMotorEx.class, "lBack");
+
+        lFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        lBack.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
 
-        forward = gamepad1.left_stick_y;
+        forward = -gamepad1.left_stick_y;
         turn = gamepad1.right_stick_x;
         strafe = gamepad1.left_stick_x;
 
-        rFront.setVelocity((forward - turn - strafe) * 300);
-        rBack.setVelocity((forward - turn + strafe) * 300);
-        lFront.setVelocity((forward + turn + strafe) * 300);
-        lBack.setVelocity((forward + turn - strafe) * 300);
+        rFront.setVelocity((forward - turn - strafe) * speed);
+        rBack.setVelocity((forward - turn + strafe) * speed);
+        lFront.setVelocity((forward + turn + strafe) * speed);
+        lBack.setVelocity((forward + turn - strafe) * speed)0;
 
+        telemetry.addData("Forward",forward);
+        telemetry.addData("Turn", turn);
+        telemetry.addData("Strafe", strafe);
+
+        telemetry.update();
     }
 
 }
