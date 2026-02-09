3aa1f518bdbb0e63eed1ea427f3e66b2e11c9c5b
RDPSRobotics <rdpsrobotics20629@gmail.com>
2026-02-09 15:54:53 -0800

    Webcam Code - April Tag Detection
    - To see April tag

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WebcamOpMode.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.Mechanism.Webcam;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+@Autonomous
+public class WebcamOpMode extends OpMode {
+
+    Webcam webcam = new Webcam();
+
+    @Override
+    public void init() {
+        webcam.init(hardwareMap, telemetry);
+    }
+
+    @Override
+    public void loop() {
+        webcam.update();
+        AprilTagDetection id20 = webcam.getTagSpecificId(20);
+        webcam.displayDetectionTelemetry(id20);
+
+    }
+
+}
