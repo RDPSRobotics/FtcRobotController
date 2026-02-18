2c9b640f588c8b9ffb8ed24369d8f4c54d6f2ff1
Shinto <84257893+icytheprogram@users.noreply.github.com>
2026-02-17 17:48:22 -0800

    Automated Intake
    - Code for testing automated intake

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoIntakeTest.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Mechanism.DistanceSensorMechanism;
+
+@TeleOp
+public class AutoIntakeTest extends OpMode {
+
+    DistanceSensorMechanism distanceSensor = new DistanceSensorMechanism();
+
+    @Override
+    public void init() {
+        distanceSensor.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        if (distanceSensor.getDistancea() >= 278) {
+            telemetry.addData("INTAKE STATUS:", "ON");
+        }
+        else {
+            telemetry.addData("INTAKE STATUS:", "OFF");
+        }
+
+        telemetry.addData("Distance", distanceSensor.getDistancea());
+        telemetry.update(); 
+
+    }
+
+
+}
