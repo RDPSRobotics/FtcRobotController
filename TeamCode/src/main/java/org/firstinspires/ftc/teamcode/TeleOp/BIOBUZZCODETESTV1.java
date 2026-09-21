b2820811b2cf103e38ac0657a822e35dd01eb293
Shinto <84257893+icytheprogram@users.noreply.github.com>
2026-09-21 12:31:47 -0700

    Code "In Theory" Finished
    This is just some example code we might be using.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/BIOBUZZCODETESTV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/BIOBUZZCODETESTV1.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.Mechanism.MecanumDrive;
@@ -19,6 +21,19 @@ public class BIOBUZZCODETESTV1 extends OpMode {
 
     private double flywheelMaxPower;
 
+    private final double triggerDeadzone = 0.2;
+
+    //--------------Shooter Variables-------------
+
+    private CRServo rCornerIntake;
+    private CRServo lCornerIntake;
+
+    private final double cornerIntakePower = 0.5;
+
+    private DcMotor intake;
+
+    private final double intakePower = 0.5;
+
     @Override
     public void init()
     {
@@ -26,6 +41,11 @@ public class BIOBUZZCODETESTV1 extends OpMode {
 
         flywheel = hardwareMap.get(DcMotorEx.class, "flywheel");
         flywheelMaxPower = 0.5f;
+
+        rCornerIntake = hardwareMap.get(CRServo.class, "rCornerIntake");
+        lCornerIntake = hardwareMap.get(CRServo.class, "lCornerIntake");
+
+        intake = hardwareMap.get(DcMotor.class, "intake");
     }
 
     @Override
@@ -34,9 +54,11 @@ public class BIOBUZZCODETESTV1 extends OpMode {
         setDrive();
 
         setShooter();
+
+        setIntake();
     }
 
-    public void setDrive() {
+    private void setDrive() {
 
         //Get controller Inputs
         forward = gamepad1.left_stick_y;
@@ -47,17 +69,28 @@ public class BIOBUZZCODETESTV1 extends OpMode {
 
     }
 
-    public void setShooter() {
+    private void setShooter() {
 
-        if (gamepad1.right_bumper) {
+        if (gamepad1.right_trigger > triggerDeadzone) {
 
             flywheel.setPower(flywheelMaxPower);
         }
         else {
-            
+
             flywheel.setPower(0);
         }
+    }
 
+    private void setIntake() {
+
+        if (gamepad1.right_bumper) {
+
+            rCornerIntake.setPower(cornerIntakePower);
+            lCornerIntake.setPower(cornerIntakePower);
+
+            intake.setPower(intakePower);
+
+        }
 
     }
 
