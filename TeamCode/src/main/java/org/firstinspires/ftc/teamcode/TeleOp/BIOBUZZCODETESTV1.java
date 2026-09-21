5dc1ec076841498547778d78465d46817ad3d1e3
Shinto <84257893+icytheprogram@users.noreply.github.com>
2026-09-21 12:17:15 -0700

    Flywheel Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/BIOBUZZCODETESTV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/BIOBUZZCODETESTV1.java
@@ -1,8 +1,8 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.Mechanism.MecanumDrive;
 
@@ -13,19 +13,51 @@ public class BIOBUZZCODETESTV1 extends OpMode {
     MecanumDrive drive = new MecanumDrive();
     public double forward,strafe,rotate;
 
+    //--------------Shooter Variables-------------
+
+    private DcMotorEx flywheel;
+
+    private double flywheelMaxPower;
+
     @Override
     public void init()
     {
         drive.init(hardwareMap);
+
+        flywheel = hardwareMap.get(DcMotorEx.class, "flywheel");
+        flywheelMaxPower = 0.5f;
     }
 
     @Override
     public void loop() {
 
+        setDrive();
+
+        setShooter();
+    }
+
+    public void setDrive() {
+
         //Get controller Inputs
         forward = gamepad1.left_stick_y;
         strafe = gamepad1.left_stick_x;
         rotate = gamepad1.right_stick_x;
+
+        drive.drive(forward,-strafe,rotate);
+
+    }
+
+    public void setShooter() {
+
+        if (gamepad1.right_bumper) {
+
+            flywheel.setPower(flywheelMaxPower);
+        }
+        else {
+            
+            flywheel.setPower(0);
+        }
+
 
     }
 
